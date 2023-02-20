b7e2772d1c8f24a21f31b88aa78f4ed0802e8037
211574329 <211574329@qq.com>
2023-02-20 16:19:12 +0800

    修改版本依赖,项目结构,新版依赖的相关配置

---
+++ mall-test/src/test/java/com/sike/mall/test/MallTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sike.mall.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
